e3a59d78c99de5774c271d325c19ae8f9b925d7f
Tejas Nikumbh <tejasnikumbh@gmail.com>
2018-05-13 12:51:39 +0530

    Added Keys

---
+++ source/keys/keys.js
@@ -0,0 +1,9 @@
+var ostAPIKey = "da7eebc4c08a9977475f";
+var ostSecret = "d30ce2edf2a22c71f458c3854d36e64a298eeae16d7f00559d33a8608e217861";
+var userAuthSecret = "abc123";
+
+module.exports = {
+  apiKey: ostAPIKey,
+  secret: ostSecret,
+  authSecret: userAuthSecret
+}
