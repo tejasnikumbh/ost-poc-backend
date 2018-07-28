ce13d4b6f2595ea21e8dfba7c2702d7d4aafaf36
Tejas Nikumbh <tejasnikumbh@gmail.com>
2018-07-29 02:08:02 +0530

    Update keys.js

--- source/keys/keys.js
+++ source/keys/keys.js
@@ -1,6 +1,6 @@
-var ostAPIKey = "da7eebc4c08a9977475f";
-var ostSecret = "d30ce2edf2a22c71f458c3854d36e64a298eeae16d7f00559d33a8608e217861";
-var userAuthSecret = "abc123";
+var ostAPIKey = "";
+var ostSecret = "";
+var userAuthSecret = "";
 
 module.exports = {
   apiKey: ostAPIKey,
